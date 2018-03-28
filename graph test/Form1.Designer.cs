1bb71a28a69649db8f7d66c2e2ffa6b3d1f6da1a
Jack Molyneux <rc0028407@student.riversidecollege.ac.uk>
2018-03-28 13:55:26 +0100

    Add project files.

---
+++ graph test/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace graph_test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend4 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.yxToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.yxToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.yxToolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea4.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea4);
+            legend4.Name = "Legend1";
+            this.chart1.Legends.Add(legend4);
+            this.chart1.Location = new System.Drawing.Point(222, 88);
+            this.chart1.Name = "chart1";
+            series4.ChartArea = "ChartArea1";
+            series4.Legend = "Legend1";
+            series4.Name = "Series1";
+            this.chart1.Series.Add(series4);
+            this.chart1.Size = new System.Drawing.Size(588, 404);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.yxToolStripMenuItem,
+            this.yxToolStripMenuItem1,
+            this.yxToolStripMenuItem2});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(990, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // yxToolStripMenuItem
+            // 
+            this.yxToolStripMenuItem.Name = "yxToolStripMenuItem";
+            this.yxToolStripMenuItem.Size = new System.Drawing.Size(38, 20);
+            this.yxToolStripMenuItem.Text = "y=x";
+            this.yxToolStripMenuItem.Click += new System.EventHandler(this.yxToolStripMenuItem_Click);
+            // 
+            // yxToolStripMenuItem1
+            // 
+            this.yxToolStripMenuItem1.Name = "yxToolStripMenuItem1";
+            this.yxToolStripMenuItem1.Size = new System.Drawing.Size(42, 20);
+            this.yxToolStripMenuItem1.Text = "y=x²";
+            this.yxToolStripMenuItem1.Click += new System.EventHandler(this.yxToolStripMenuItem1_Click);
+            // 
+            // yxToolStripMenuItem2
+            // 
+            this.yxToolStripMenuItem2.Name = "yxToolStripMenuItem2";
+            this.yxToolStripMenuItem2.Size = new System.Drawing.Size(42, 20);
+            this.yxToolStripMenuItem2.Text = "y=x³";
+            this.yxToolStripMenuItem2.Click += new System.EventHandler(this.yxToolStripMenuItem2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(990, 628);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem yxToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem yxToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem yxToolStripMenuItem2;
+    }
+}
+
---
+++ graph test/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace graph_test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            List<Point> squares = new List<Point>();
+            for (int i = 0; i < 11; i++)
+            {
+                squares.Add(new Point(i, i * i));
+            }
+            drawGraph(squares, "x", "x squared");
+        }
+
+        void drawGraph(List<Point> points, string XaxisTitle="x", string YaxisTitle="y")
+        {
+            chart1.Series.Clear();
+            chart1.ChartAreas[0].AxisX.IsMarginVisible = false;
+            Series series1 = new Series
+            {
+                Name = "Line",
+                Color = Color.Blue,
+                IsVisibleInLegend = false,
+                IsXValueIndexed = true,
+                ChartType = SeriesChartType.Spline,
+                BorderWidth = 2
+            };
+            Series series2 = new Series
+            {
+                Name = "Points",
+                Color = Color.Red,
+                IsVisibleInLegend = false,
+                IsXValueIndexed = true,
+                ChartType = SeriesChartType.Point,
+                BorderWidth = 2
+            };
+            chart1.Series.Add(series1);
+            chart1.Series.Add(series2);
+            for (int i = 0; i < 11; i++)
+            {
+                series1.Points.AddXY(i, i * i);
+                series2.Points.AddXY(i, i * i);
+            }
+            chart1.ChartAreas[0].AxisX.Title = "time / s";
+            chart1.ChartAreas[0].AxisY.Title = "distance / m";
+        }
+
+        public void yxToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            List<Point> xline = new List<Point>();
+            for (int i = 0; i < 11; i++)
+            {
+                xline.Add(new Point(i, i));
+            }
+            drawGraph
+        }
+
+        private void yxToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            List<Point> xline = new List<Point>();
+            for (int i = 0; i < 11; i++)
+            {
+                xline.Add(new Point(i, i * i));
+            }
+            drawGraph
+        }
+
+        private void yxToolStripMenuItem2_Click(object sender, EventArgs e)
+        {
+            List<Point> xline = new List<Point>();
+            for (int i = 0; i < 11; i++)
+            {
+                xline.Add(new Point(i, i * i * i));
+            }
+            drawGraph
+        }
+    }
+}
